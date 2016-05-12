3e3afdefb5f74c4416fc7eea74fe1c1bfc269fec
Ewa Zebrowska <ewazebrowska4@gmail.com>
2016-05-12 21:23:10 +0200

    Initial Commit

---
+++ PodTestE/PodTestE.h
@@ -0,0 +1,19 @@
+//
+//  PodTestE.h
+//  PodTestE
+//
+//  Created by Ewa Zebrowska on 12/05/16.
+//  Copyright © 2016 Handy Software Ewa Żebrowska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PodTestE.
+FOUNDATION_EXPORT double PodTestEVersionNumber;
+
+//! Project version string for PodTestE.
+FOUNDATION_EXPORT const unsigned char PodTestEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodTestE/PublicHeader.h>
+
+
